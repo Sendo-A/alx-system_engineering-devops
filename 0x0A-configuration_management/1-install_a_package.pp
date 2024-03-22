c61ad7e73f64eb4b2de326d7451ae79e2301a1a8
CHAJIA ABDELHADI <abdelhadi-chajia@outlook.fr>
2024-03-22 16:55:50 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Installs flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+# manifest.pp
+
+# Define package resource for Python3 pip
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Define exec resource to install Flask using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
