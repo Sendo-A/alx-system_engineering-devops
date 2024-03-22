e3f21022751b2beb232bdfc188663848f1863471
CHAJIA ABDELHADI <abdelhadi-chajia@outlook.fr>
2024-03-22 16:59:46 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# manifest.pp
-
-# Define package resource for Python3 pip
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Define exec resource to install Flask using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+# Installs a package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
