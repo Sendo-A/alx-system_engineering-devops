1dc150721904b9e8b2ceb5101d205cc0f7b19427
CHAJIA ABDELHADI <abdelhadi-chajia@outlook.fr>
2024-03-22 17:01:41 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
-# Installs a package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# 1-install_a_package.pp
+
+# Ensure the python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+# Notify the installed Flask version
+notify { 'Flask_installed':
+  message => 'Flask 2.1.0 has been installed successfully',
+  require => Exec['install_flask'],
 }
